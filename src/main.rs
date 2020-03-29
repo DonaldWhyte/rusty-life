68992b65a8dc31578f78bbaf0b7d532f55575a8f
Donald Whyte <donaldwhyte0@gmail.com>
2020-03-29 17:23:27 +0100

    Add ability to configure fps

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+extern crate docopt;
 extern crate rand;
 extern crate term_size;
 
 mod grid;
 
-fn run(width: usize, height: usize) {
+const USAGE: &'static str = "
+Usage: rusty-life [options]
+
+Options:
+    --fps=<fps>  Frames/grid updates to render/run per second [default: 5].
+";
+
+fn main() {
+    let args = docopt::Docopt::new(USAGE)
+        .and_then(|d| d.parse())
+        .unwrap_or_else(|e| e.exit());
+
+    let fps = args.get_str("--fps").parse::<u64>().unwrap();
+
+    if let Some((width, height)) = term_size::dimensions() {
+      run(width, height, fps);
+    } else {
+      println!("Could not determine terminal width and height. Aborting.")
+    }
+}
+
+fn run(width: usize, height: usize, fps: u64) {
     let mut rng = rand::thread_rng();
 
     let width_without_borders = width - 2;
@@ -15,23 +37,14 @@ fn run(width: usize, height: usize) {
         height_without_borders,
         &mut rng);
 
+    let sleep_interval = std::time::Duration::from_millis(1000 / fps);
+
     loop {
         static CLEAR_TERMIAL_CONTROL_CHAR: &str = "\x1B[2J";
         print!("{}{}", CLEAR_TERMIAL_CONTROL_CHAR, grid);
 
-        static SLEEP_INTERVAL: std::time::Duration =
-            std::time::Duration::from_millis(1000 / 5); // 5 FPS
-        std::thread::sleep(SLEEP_INTERVAL);
+        std::thread::sleep(sleep_interval);
 
         grid = grid.update();
     }
-}
-
-
-fn main() {
-  if let Some((width, height)) = term_size::dimensions() {
-    run(width, height);
-  } else {
-    println!("Could not determine terminal width and height. Aborting.")
-  }
 }
